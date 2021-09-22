db183c4964596fa3a6ea543a06cae3852527c90b
Rahat-Khan-Pathan <rahatkhanpathan99@gmail.com>
2021-09-22 18:28:50 +0600

    footer update

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css';
 
 const Footer = () => {
     return (
-        <div className="footer-container container">
+        <div className="footer-container">
             <p className="credit">©Rahat Khan Pathan. All rights reserved.</p>
         </div>
     );
