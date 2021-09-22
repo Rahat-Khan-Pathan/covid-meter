240e4bc050634019f1415c0e16ad386c8aac8783
Rahat-Khan-Pathan <rahatkhanpathan99@gmail.com>
2021-09-22 09:41:28 +0600

    bug fixed

--- src/Components/Chart/Chart.js
+++ src/Components/Chart/Chart.js
@@ -10,71 +10,90 @@ import {
 } from "recharts";
 
 export default function App(props) {
-    useEffect(()=> {
-        window.scrollTo(0, 0);
-    },[]);
-  const { data,vacData } = props;
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+  const { data, vacData } = props;
   return (
-    <div className="chart-container">
-      <div>
-        <AreaChart
-          width={800}
-          height={500}
-          data={data}
-          syncId="anyId"
-          margin={{
-            top: 10,
-            right: 30,
-            left: 0,
-            bottom: 0,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Area type="monotone" dataKey="uv" stroke="lightcyan" fill="steelblue" />
-        </AreaChart>
-        <h4 className="text-center mb-5">Affected Cases Growth Chart</h4>
-        <AreaChart
-          width={800}
-          height={500}
-          data={data}
-          syncId="anyId"
-          margin={{
-            top: 10,
-            right: 30,
-            left: 0,
-            bottom: 0,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Area type="monotone" dataKey="pv" stroke="red" fill="orangered" />
-        </AreaChart>
-        <h4 className="text-center mb-5">Deaths Growth Chart</h4>
-        <AreaChart
-          width={800}
-          height={500}
-          data={vacData}
-          syncId="anyId"
-          margin={{
-            top: 10,
-            right: 30,
-            left: 0,
-            bottom: 0,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Area type="monotone" dataKey="cv" stroke="green" fill="lightgreen" />
-        </AreaChart>
-        <h4 className="text-center mb-5">Vaccination Growth Chart</h4>
-      </div>
-    </div>
+    <>
+      {data.length > 0 && (
+        <div className="chart-container">
+          <div>
+            <AreaChart
+              width={800}
+              height={500}
+              data={data}
+              syncId="anyId"
+              margin={{
+                top: 10,
+                right: 30,
+                left: 0,
+                bottom: 0,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              <Area
+                type="monotone"
+                dataKey="uv"
+                stroke="lightcyan"
+                fill="steelblue"
+              />
+            </AreaChart>
+            <h4 className="text-center mb-5">Affected Cases Growth Chart</h4>
+            <AreaChart
+              width={800}
+              height={500}
+              data={data}
+              syncId="anyId"
+              margin={{
+                top: 10,
+                right: 30,
+                left: 0,
+                bottom: 0,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              <Area
+                type="monotone"
+                dataKey="pv"
+                stroke="red"
+                fill="orangered"
+              />
+            </AreaChart>
+            <h4 className="text-center mb-5">Deaths Growth Chart</h4>
+            <AreaChart
+              width={800}
+              height={500}
+              data={vacData}
+              syncId="anyId"
+              margin={{
+                top: 10,
+                right: 30,
+                left: 0,
+                bottom: 0,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              <Area
+                type="monotone"
+                dataKey="cv"
+                stroke="green"
+                fill="lightgreen"
+              />
+            </AreaChart>
+            <h4 className="text-center mb-5">Vaccination Growth Chart</h4>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
